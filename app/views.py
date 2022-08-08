1ee0d944b49abbac40675d283d6076467aab5528
teesha-13 <94960535+teesha-13@users.noreply.github.com>
2022-08-09 00:07:45 +0530

    basic setup done

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import *
 
 # Create your views here.
+
+def Chat(request):
+    return render(request, "app/base.html")
